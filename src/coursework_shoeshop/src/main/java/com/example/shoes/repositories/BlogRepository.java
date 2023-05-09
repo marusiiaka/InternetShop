8b8d0559aab91d4ef33fb3c7f45c21723e2e0af6
marusiiaka <marusia2003@mail.ru>
2023-05-09 22:54:36 +0300

    sec

--- src/coursework_shoeshop/src/main/java/com/example/shoes/repositories/BlogRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.shoes.repositories;
-
-import com.example.shoes.models.Blog;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BlogRepository extends JpaRepository<Blog,Long> {
-}
